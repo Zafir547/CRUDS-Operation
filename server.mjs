991ad1b4311f7129cdfc4675b4faa7d64dd1d694
Shaheer Shahzad <mohammedshaheershahzadrawda5@gmail.com>
2023-07-27 16:31:34 +0500

    complete crud app

--- server.mjs
+++ server.mjs
@@ -1,7 +1,7 @@
 import express from "express";
 import { customAlphabet } from 'nanoid'
 const nanoid = customAlphabet('1234567890', 20)
-import { MongoClient } from "mongodb"
+import { MongoClient, ObjectId } from "mongodb"
 
 import './config/index.mjs'
 
@@ -20,15 +20,6 @@ app.get("/", (req, res) => {
 });
 
 
-let products = [
-    {
-        id: nanoid(), // Always a number
-        name: "Laptop",
-        price: "$300",
-        description: "The Laptop is a good product"
-    }
-];
-
 app.get("/products", async (req, res) => {
     console.log("get running");
     const result = await productCollection.find({}).toArray()
@@ -45,42 +36,12 @@ app.get("/products", async (req, res) => {
 
 //  https://baseurl.com/product/1231
 app.get("/product/:id", (req, res) => {
-    console.log(typeof req.params.id)
 
-    if (isNaN(req.params.id)) {
-        res.status(403).send("invalide product id");
-    }
 
-    let isFound = false;
-
-    for (let i = 0; i < products.length; i++) {
-        if (products[i].id === req.params.id) {
-            isFound = i;
-            break;
-        }
-    }
-
-    if (isFound == false) {
-        res.status(404);
-        res.send({
-            message: "product not found"
-        });
-    } else {
-        res.send({
-            message: "product not found with id: " + products[isFound].id,
-            data: products[isFound]
-        });
-    }
 });
 
 app.post("/product", async (req, res) => {
-    // {
-    //     id: 212342, // Always a number
-    //     name: "AI product",
-    //     price: "$50.25",
-    //     description: "AI product description"
-    // }
-
+    
     if (!req.body.name
         || !req.body.price
         || !req.body.description) {
@@ -110,7 +71,7 @@ app.post("/product", async (req, res) => {
 
 });
 
-app.put("/product/:id", (req, res) => {
+app.put("/product/:id", async (req, res) => {
 
     if (
         !req.body.name
@@ -128,58 +89,37 @@ app.put("/product/:id", (req, res) => {
           }`);
     }
 
-
-
-    let isFound = false;
-
-    for (let i = 0; i < products.length; i++) {
-        if (products[i].id === req.params.id) {
-            isFound = i;
-            break;
-        }
+await productCollection.findOneAndUpdate({_id:new ObjectId(req.params.id)},{
+    $set:{
+        name: req.body.name,
+        price: req.body.price,
+        description: req.body.description,
     }
+}).then(()=>{
+ res.status(200).json({
+    message:"Product Updated Successfully"
+ })
+}).catch((error)=>{
+    res.status(500).json({
+        error
+     })
+})
 
-    if (isFound === false) {
-        res.status(404);
-        res.send({
-            message: "product not found"
-        });
-    } else {
-        if (req.body.name) products[isFound].name = req.body.name
-        if (req.body.price) products[isFound].price = req.body.price
-        if (req.body.description) products[isFound].description = req.body.description
-
-        res.send({
-            message: "product is updated with id: " + products[isFound].id,
-            data: products[isFound]
-        });
-    }
 
 });
 
-app.delete("/product/:id", (req, res) => {
-
-    let isFound = false;
-
-    for (let i = 0; i < products.length; i++) {
-        if (products[i].id === req.params.id) {
-            isFound = i;
-            break;
-        }
-    }
-
-    if (isFound === false) {
-        res.status(404);
-        res.send({
-            message: "product not found"
-        });
-    } else {
-        products.splice(isFound, 1)
-
-        res.send({
-            message: "product is deleted"
-        });
-    }
+app.delete("/product/:id", async (req, res) => {
+         const {id}  = req.params
+  await  productCollection.findOneAndDelete({_id:new ObjectId(id)}).then(()=>{
+    res.status(200).json({
+        message:"Product Deleted Successfully"
+     })
+  }).catch((error)=>{
+    res.status(500).json({
+        error
+     })
+  })
+    
 });
 
 
